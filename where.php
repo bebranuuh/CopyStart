6712986d15cd2acea8b71d4aba2d15590de08b8d
Azamat Toxanbayev <h7559006@gmail.com>
2024-06-21 20:58:17 +0500

    message

---
+++ where.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include "header.php";
+?>
+
+<main>
+	<div class="content">
+
+		<div class="head">Наши контакты</div>
+		<p>Адрес: <b>ул. Лермонтова 93</b></p>
+		<p>Номер телефона: <b>8 777 777 77 77</b></p>
+		<p>Email: <b>pavlodar@gmail.com</b></p>
+
+		<div class="head">Наше местоположение</div>
+		<img src="images/map.png" alt="">
+
+	</div>
+</main>
+
+<?php include "footer.php" ?>
